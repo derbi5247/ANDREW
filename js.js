4a2b3c2c947527c5062597652660693aae4f4739
derbi5247 <malakaevdenis@gmail.com>
2021-12-07 23:39:40 +0200

    m

--- js.js
+++ js.js
@@ -1,10 +1,18 @@
-$(document).ready(function(){
-    $('.menu__toggle').click(function(){
-        $(this).toggleClass('active');
-        $('.preview__text-top').toggleClass('active')
-        $('.preview__text-bot').toggleClass('active')
-        $('.header__items').slideToggle(300);
+// $(document).ready(function(){
+//     $('.menu__toggle').click(function(){
+//         $(this).toggleClass('active')
+//         $('.header__items').slideToggle(300);
+//     })
+// })
+
+
+$(document).ready(function() {
+    $('.burger').click(function(){
+        $(this).toggleClass('active')
+        $('.nav__links').toggleClass('active')
+        $('.burger__line').toggleClass('active')
+        $('body').toggleClass('active');
     })
 })
 
-
+// let burger = document.querySelector('burger')
